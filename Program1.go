4cb6401c97c5c872e044fa51312fd95d9924d93d
Yuki Hagihara <m213879@hiroshima-u.ac.jp>
2021-06-23 02:07:00 +0000

    completed file

--- Program1.go
+++ Program1.go
@@ -1,13 +1,39 @@
 package main
 
 import (
-//	"fmt"
-//	"strconv"
-//	"strings"
+	"fmt"
+	//	"strconv"
+	"strings"
 )
 
 func inttoroman(i int) string {
-	var roman string = "XIV"
+
+	var roman string
+	alabtable := []int{4000, 3000, 2000, 1000, 900, 800, 700, 600, 500, 400, 300, 200, 100, 90, 80, 70, 60, 50, 40, 30, 20, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1}
+	romantable := []string{"MMMM", "MMM", "MM", "M", "CM", "DCCC", "DCC", "DC", "D", "CD", "CCC", "CC", "C", "XC", "LXXX", "LXX", "LX", "L", "XL", "XXX", "XX", "X", "IX", "VIII", "VII", "VI", "V", "IV", "III", "II", "I"}
+
+	tablesize := 31
+	var romanresult []string
+	digit := 0
+
+	for j := 0; j < tablesize; j++ {
+		result := i / alabtable[j]
+
+		if result == 1 {
+
+			romanresult = append(romanresult, romantable[j])
+			digit++
+			i = i - alabtable[j]
+		}
+	}
+	roman = strings.Join(romanresult, "")
 
 	return roman
+}
+
+func main() {
+	x := 1990
+
+	fmt.Println(inttoroman(x))
+
 }
--- Program1_test.go
+++ Program1_test.go
@@ -9,3 +9,43 @@ func TestEx01(t *testing.T) {
 		t.Error("Test01 is failed")
 	}
 }
+
+func TestEx02(t *testing.T) {
+	expect := "MCMXC"
+	result := inttoroman(1990)
+	if result != expect {
+		t.Error("Test02 is failed")
+	}
+}
+
+func TestEx03(t *testing.T) {
+	expect := "MMVIII"
+	result := inttoroman(2008)
+	if result != expect {
+		t.Error("Test03 is failed")
+	}
+}
+
+func TestEx04(t *testing.T) {
+	expect := "XCIX"
+	result := inttoroman(99)
+	if result != expect {
+		t.Error("Test04 is failed")
+	}
+}
+
+func TestEx05(t *testing.T) {
+	expect := "XLVII"
+	result := inttoroman(47)
+	if result != expect {
+		t.Error("Test05 is failed")
+	}
+}
+
+func TestEx06(t *testing.T) {
+	expect := "XLII"
+	result := inttoroman(42)
+	if result != expect {
+		t.Error("Test06 is failed")
+	}
+}
